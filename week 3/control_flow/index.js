e281319048d489bf99727ea6b5eea32fa070b9f5
irsad <proirsad21@gmail.com>
2021-09-13 23:02:22 +0700

    telah mengerjakan javasctipt control flow

---
+++ week 3/control_flow/index.js
@@ -0,0 +1,20 @@
+let nilai = prompt('masukan nilai anda 1 - 100 :');
+if (nilai >= 101) {
+    alert('Nilai anda jangan lebih dari 100 !!!!!!!');
+} else if (nilai >= 90 && nilai <= 100) {
+    alert('nilai anda A');
+} else if (nilai >= 71 && nilai <= 89) {
+    alert('nilai anda B');
+} else if (nilai >= 51 && nilai <= 70) {
+    alert('nilai anda C');
+} else if (nilai >= 31 && nilai <= 50) {
+    alert('nilai anda D');
+} else if (nilai <= 30 && nilai >= 1) {
+    alert('nilai anda E');
+} else if (nilai <= 0) {
+    alert('Harus lebih dari 0');
+} else if (nilai = String) {
+    alert('harus memasukan angka');
+} else {
+    alert('harus memasukan nilai')
+}
